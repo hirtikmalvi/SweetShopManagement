12edc1fe18107851d23896116b39160ed869a9aa
Hirtik Malvi <hirtik.malvi@gmail.com>
2025-12-13 19:57:37 +0530

    chore: database setup using EF Core and SQL Server

---
+++ backend/SweetShop.Api/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SweetShop.Api.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+    }
+}
