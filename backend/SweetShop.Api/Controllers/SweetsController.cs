c98c97a187ebdbb5dd6c00aba68c48a730bf5fe9
Hirtik Malvi <hirtik.malvi@gmail.com>
2025-12-15 20:43:48 +0530

    feat: add route for creating sweet and apply setup of authentication and authorization

---
+++ backend/SweetShop.Api/Controllers/SweetsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SweetShop.Api.DTOs.Register;
+using SweetShop.Api.DTOs.Sweet;
+using SweetShop.Api.Entities;
+using SweetShop.Api.Services.Implementations;
+using SweetShop.Api.Services.Interfaces;
+using SweetShop.Api.Shared;
+
+namespace SweetShop.Api.Controllers
+{
+    [Route("api/sweets")]
+    [ApiController]
+    public class SweetsController : ControllerBase
+    {
+        private readonly ISweetsService sweetsService;
+
+        public SweetsController(ISweetsService _sweetsService)
+        {
+            sweetsService = _sweetsService;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> CreateSweet([FromBody] CreateSweetRequestDTO request)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Where(ms => ms.Value.Errors.Count > 0).SelectMany(kvp => kvp.Value.Errors.Select(err => err.ErrorMessage)).ToList();
+                return Ok(CustomResult<Sweet>.Fail("Sweet Creation Failed.", 400, errors));
+            }
+            var result = await sweetsService.CreateSweet(request);
+            return Ok(result);
+        }
+    }
+}
--- backend/SweetShop.Api/Program.cs
+++ backend/SweetShop.Api/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 using SweetShop.Api.Data;
 using SweetShop.Api.Helpers;
@@ -7,12 +9,58 @@ using SweetShop.Api.Repositories.Implementations;
 using SweetShop.Api.Repositories.Interfaces;
 using SweetShop.Api.Services.Implementations;
 using SweetShop.Api.Services.Interfaces;
+using SweetShop.Api.Shared;
+using System.Text;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = builder.Configuration.GetValue<string>("JwtSettings:Issuer"),
+        ValidAudience = builder.Configuration.GetValue<string>("JwtSettings:Audience"),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("JwtSettings:Key")!)),
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+    };
+    options.Events = new JwtBearerEvents()
+    {
+        // Authentication
+        OnChallenge = context =>
+        {
+            context.HandleResponse();
+            context.Response.StatusCode = 401;
+            context.Response.ContentType = "application/json";
+
+            var result = JsonSerializer.Serialize(CustomResult<string>.Fail("Authentication Failed.", 401, ["Token is missing or invalid."]), new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            });
+            return context.Response.WriteAsync(result);
+        },
+        // Authorisation
+        OnForbidden = context =>
+        {
+            context.Response.StatusCode = 403;
+            context.Response.ContentType = "application/json";
+
+            var result = JsonSerializer.Serialize(CustomResult<string>.Fail("Access denied.", 403, ["You are not authorised."]), new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+            return context.Response.WriteAsync(result);
+        }
+    };
+});
+
 
 builder.Services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
 
@@ -25,10 +73,12 @@ builder.Services.AddOpenApi();
 
 // Repositories
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+builder.Services.AddScoped<ISweetsRepository, SweetsRepository>();
 
 // Services
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<ISweetsService, SweetsService>();
 builder.Services.AddScoped<ICurrentUserContext, CurrentUserContext>();
 
 var app = builder.Build();
