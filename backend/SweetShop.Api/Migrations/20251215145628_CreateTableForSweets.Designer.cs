446407888182b4b14bdff748538f9158aa598f0c
Hirtik Malvi <hirtik.malvi@gmail.com>
2025-12-15 20:42:12 +0530

    chore: create table for sweets

--- backend/SweetShop.Api/Migrations/20251215145628_CreateTableForSweets.Designer.cs
+++ backend/SweetShop.Api/Migrations/20251215145628_CreateTableForSweets.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SweetShop.Api.Data;
 
@@ -10,9 +11,11 @@ using SweetShop.Api.Data;
 namespace SweetShop.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251215145628_CreateTableForSweets")]
+    partial class CreateTableForSweets
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/SweetShop.Api/Migrations/20251215145628_CreateTableForSweets.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SweetShop.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableForSweets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sweets",
+                columns: table => new
+                {
+                    SweetId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    QuantityInStock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sweets", x => x.SweetId);
+                    table.CheckConstraint("CK_Sweet_Price", "[Price] >= 0.0");
+                    table.CheckConstraint("CK_Sweet_QuantityInStock", "[QuantityInStock] >= 1");
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sweets");
+        }
+    }
+}
