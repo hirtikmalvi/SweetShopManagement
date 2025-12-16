4d93e4baa3b6cb33c50e0df1c4fb40d5784df708
Hirtik Malvi <hirtik.malvi@gmail.com>
2025-12-16 12:24:35 +0530

    fix: make sweet id required field

--- backend/SweetShop.Api/DTOs/Sweet/UpdateSweetStockRequestDTO.cs
+++ backend/SweetShop.Api/DTOs/Sweet/UpdateSweetStockRequestDTO.cs
@@ -4,6 +4,7 @@ namespace SweetShop.Api.DTOs.Sweet
 {
     public class UpdateSweetStockRequestDTO
     {
+        [Required]
         public int SweetId { get; set; }
         [Required]
         [Range(1, int.MaxValue, ErrorMessage = "Quantity must be atleast 1.")]
