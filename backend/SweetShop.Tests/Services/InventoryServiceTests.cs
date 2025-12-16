24581749e920a039878d174cf4d95e46f5aa3cef
Hirtik Malvi <hirtik.malvi@gmail.com>
2025-12-16 10:18:47 +0530

    test: add failing test to delete sweet when sweetid in route and body mismatch

---
+++ backend/SweetShop.Tests/Services/InventoryServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using SweetShop.Api.DTOs.Sweet;
+using SweetShop.Api.Entities;
+using SweetShop.Api.Repositories.Interfaces;
+using SweetShop.Api.Services.Implementations;
+using SweetShop.Api.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweetShop.Tests.Services
+{
+    public class InventoryServiceTests
+    {
+        private readonly Mock<ISweetsRepository> sweetsRepo;
+        private readonly IInventoryService inventoryService;
+        public InventoryServiceTests()
+        {
+            sweetsrepo = new Mock<ISweetsRepository>();
+            inventoryService = new IInventoryService(sweetsRepo.Object);
+        }
+        [Fact]
+        public async Task UpdateSweet_ShouldFail_WhenRouteIdDoesNotMatchRequestId()
+        {
+            // Arrange
+            var routeSweetId = 1;
+
+            var request = new UpdateSweetStockRequestDTO
+            {
+                SweetId = 2,
+                QuantityInStock = 120
+            };
+
+            // Act
+            var result = await inventoryService.PurchaseSweet(routeSweetId, request);
+
+            // Assert
+            Assert.False(result.Success);
+            Assert.Equal(400, result.StatusCode);
+            Assert.Null(result.Data);
+            Assert.Equal("SweetId mismatch.", result.Message);
+        }
+    }
+}
